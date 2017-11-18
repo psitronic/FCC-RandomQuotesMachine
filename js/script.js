44d8a863f7209a62068d5a5634d8fdd8e3fb4381
Andrey Sidorenko <spintronic@tuta.io>
2017-11-18 20:03:23 +0100

    Added button for sharing a quote

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
+var convertQuote = function (post){
+    post.content = post.content.replace(/(<([^>]+)>)/ig, "");
+    post.link = post.content.replace(/&#'(\d+);/g, function(m, n) {
+        return String.fromCharCode(n);
+    });
+    post.link = encodeURI(post.content + " -" + post.title);
+    return post;
+}
+
 $.ajaxSetup({cache: false});
 $(document).ready(function() {
-    $('#getQuote').on('click', function(e) {
+    $('#getQuote').on('click', function() {
         $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(data) {
             var post = data.shift();
-            $("#quote-content").html(post.content);
-            $("#quote-title").text(post.title);
+            post = convertQuote(post);
+            var author = post.title;
+            var quote = post.content;
+            
+            $("#quote-content").html(quote);
+            $("#quote-title").html(author);
+            $("#twit-quote").attr("href", "https://twitter.com/intent/tweet?text=" + post.link);
         });
     });
 });
